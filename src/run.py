0f5bb5a9639f06792785e16861c038fd642c7615
StevePirateX <sharkbait__@hotmail.com>
2021-07-09 10:34:34 +1000

    Added processing log times to console

--- src/run.py
+++ src/run.py
@@ -3,18 +3,24 @@ import constants as c
 from classes import VatsimController
 from datetime import datetime
 import threading
+import time
+
+loop_end_time = 0
+afv_data = None
 
 
-def refresh_loop(afv_counter: int):
+def refresh_loop(fetch_afv: bool):
     global afv_data
-    if afv_counter > 10:
+    global loop_end_time
+    if fetch_afv or afv_data is None:
+        log_print('Fetching AFV Data')
         afv_data = f.get_json_from_url(f.get_afv_url())
-    print()
-    print("Refresh time: {}".format(datetime.now().isoformat()[:-7]))
+    log_print('Fetching VATSIM Data')
     VatsimController.copy_controllers()
     f.add_controller_coordinates(afv_data)
-    print("Online controllers:")
     f.get_vatsim_controllers()
+    log_print('VATSIM data loaded')
+    log_print('Online controllers:')
 
     local_online_controllers = []
     callsign_max_length = 0
@@ -27,39 +33,77 @@ def refresh_loop(afv_counter: int):
                 callsign_max_length = len(controller.callsign)
 
     if len(local_online_controllers) > 0:
-        print('{0:{1}}'.format("Callsign", callsign_max_length),
-              ' ',
-              '{0:20}'.format("Name"),
-              '{}'.format("Online Time"),
-              )
+        header = '{0:{1}} {2:20} {3}'.format('Callsign', callsign_max_length,
+                                             'Name', 'Online Time')
+        log_print(header)
         local_online_controllers.sort(key=lambda x: x.callsign)
         for controller in local_online_controllers:
             session_time = controller.get_session_time()
             hours = str(session_time[0]).zfill(2)
             minutes = str(session_time[1]).zfill(2)
-            print('{0:{1}}'.format(controller.callsign, callsign_max_length),
-                  '-',
-                  '{0:20}'.format(controller.name),
-                  '{}:{}'.format(hours, minutes),
-                  )
+            controller_output = '{0:{1}} - {2:20} {3}:{4}'.format(
+                controller.callsign, callsign_max_length, controller.name,
+                hours, minutes)
+            log_print(controller_output)
     else:
-        print("No controllers found")
+        log_print("No controllers found")
     VatsimController.clean_controllers()
+    loop_end_time = time.time()
+
+
+def log_print(label: str):
+    print('{}: {}'.format(datetime.now().strftime('%H:%M:%S.%f')[:12], label))
+
+
+def average_run_calc(total_loop_time, total_runs) -> float:
+    return round(total_loop_time / total_runs, 3)
 
 
 if __name__ == "__main__":
-    print("VATSIM Online Controllers v{}".format(c.VERSION))
+    log_print('Starting')
+    log_print("VATSIM Online Controllers v{}".format(c.VERSION))
     f.import_config(c.CONFIG_FILENAME)
+    log_print('Config loaded')
     afv_api_url = f.get_afv_url()
-    afv_data = f.get_json_from_url(afv_api_url)
+    # afv_data = f.get_json_from_url(afv_api_url)
+    # log_print('AFV data loaded')
     atc_positions = f.get_atc_positions()
+    log_print('POF file loaded')
 
     event = threading.Event()
     afv_update_counter = 0
+
+    afv_refreshes = 0
+    afv_total_run_time = 0
+    afv_avg_refresh = 0
+    nonafv_refreshes = 0
+    nonafv_total_run_time = 0
+    nonafv_avg_refresh = 0
     while True:
         try:
+            print()
+            log_print('Refreshing...')
+            loop_start_time = time.time()
+            if afv_update_counter % 3 == 0:
+                refresh_loop(True)
+                afv_refreshes += 1
+                afv_total_run_time += round(loop_end_time - loop_start_time, 3)
+                afv_avg_refresh = average_run_calc(afv_total_run_time,
+                                                   afv_refreshes)
+            else:
+                refresh_loop(False)
+                nonafv_refreshes += 1
+                nonafv_total_run_time += round(loop_end_time - loop_start_time,
+                                               3)
+                nonafv_avg_refresh = average_run_calc(nonafv_total_run_time,
+                                                      nonafv_refreshes)
             afv_update_counter += 1
-            refresh_loop(afv_update_counter)
+            loop_running_time = round(loop_end_time - loop_start_time, 3)
+            log_print('Refresh run time = {}s'.format(loop_running_time))
+            log_print('Average refresh time w/o AFV = {}s'.format(
+                nonafv_avg_refresh))
+            log_print(
+                'Average refresh time with AFV= {}s'.format(afv_avg_refresh))
             event.wait(c.REFRESH_TIME)
         except KeyboardInterrupt:
             event.set()
